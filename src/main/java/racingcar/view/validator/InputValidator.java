9757cd9fd7317631a733f8ed6f4a498c80387ec8
Zrfv <youngoh07015@gmail.com>
2024-10-23 20:21:47 +0900

    feat: 각 자동차 이름이 5글자 초과 또는 공백일 경우 예외 처리 구현

--- src/main/java/racingcar/view/validator/InputValidator.java
+++ src/main/java/racingcar/view/validator/InputValidator.java
@@ -3,7 +3,9 @@ package racingcar.view.validator;
 public class InputValidator {
 
     private final String NOT_MULTIPLE_CAR = "자동차는 2개 이상 입력해주세요.";
+    private final String EMPTY_STRING = "자동차 이름에 공백은 입력할 수 없습니다.";
     private final String NOT_POSITIVE_NUMBER = "1 이상의 정수만 입력가능합니다.";
+    private final String INVALID_CAR_NAME = "자동차 이름은 1글자에서 5글자 사이로 입력가능합니다.";
 
     public void checkSingleCar(String input) { // 쉼표 있는지 확인, 없으면 차 한대이므로 예외
         if (!input.contains(",")) {
@@ -13,13 +15,27 @@ public class InputValidator {
 
     public void checkEmptyString(String input) {
         if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException(NOT_MULTIPLE_CAR);
+            throw new IllegalArgumentException(EMPTY_STRING);
         }
     }
 
     public void checkPositiveNumber(Integer input) {
         if (!(input >= 1)) {
             throw new IllegalArgumentException(NOT_POSITIVE_NUMBER);
+        }
+    }
+
+    public void checkEmptyString(String[] input) {
+        for (String name : input) {
+            checkEmptyString(name);
+        }
+    }
+
+    public void checkCarNameLength(String[] input) {
+        for (String name : input) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException(INVALID_CAR_NAME);
+            }
         }
     }
 }
--- src/test/java/racingcar/view/validator/InputValidatorTest.java
+++ src/test/java/racingcar/view/validator/InputValidatorTest.java
@@ -18,11 +18,11 @@ class InputValidatorTest{
     void 입력_문자열_공백_예외_테스트() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> new InputValidator().checkEmptyString(""));
-        assertEquals("자동차는 2개 이상 입력해주세요.", exception.getMessage());
+        assertEquals("자동차 이름에 공백은 입력할 수 없습니다.", exception.getMessage());
 
         exception = assertThrows(IllegalArgumentException.class,
                 () -> new InputValidator().checkEmptyString("  \n \t "));
-        assertEquals("자동차는 2개 이상 입력해주세요.", exception.getMessage());
+        assertEquals("자동차 이름에 공백은 입력할 수 없습니다.", exception.getMessage());
     }
 
     @Test
@@ -36,5 +36,25 @@ class InputValidatorTest{
         assertEquals("1 이상의 정수만 입력가능합니다.", exception.getMessage());
     }
 
+    @Test
+    void 자동차_이름이_공백이면_예외_테스트() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new InputValidator().checkEmptyString(new String[]{"kim", "lee","    "}));
+        assertEquals("자동차 이름에 공백은 입력할 수 없습니다.", exception.getMessage());
 
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> new InputValidator().checkEmptyString(new String[]{"kim","lee",""}));
+        assertEquals("자동차 이름에 공백은 입력할 수 없습니다.", exception.getMessage());
+    }
+
+    @Test
+    void 자동차_이름이_5글자_초과하면_예외_테스트() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new InputValidator().checkCarNameLength(new String[]{"kim", "lee","     alpha    "}));
+        assertEquals("자동차 이름은 1글자에서 5글자 사이로 입력가능합니다.", exception.getMessage());
+
+        exception = assertThrows(IllegalArgumentException.class,
+                () -> new InputValidator().checkCarNameLength(new String[]{"kim","lee","The Weeknd"}));
+        assertEquals("자동차 이름은 1글자에서 5글자 사이로 입력가능합니다.", exception.getMessage());
+    }
 }
